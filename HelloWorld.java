12ad1f4d6357a8035a6bfb560b9dfdc562907379
PrakashYalavatti <prakashyalavatti21@gmail.com>
2023-11-04 17:39:30 +0530

    Update HelloWorld.java
    Added division code

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,10 @@
 public class HelloWorld {
   public static void main(String[] args){
     System.out.println("Hello, World");
+    int a = 5;
+    int b = 5;
+
+    int div = a/b;
+    System.out.println(" Div " + div);
   }
 }
